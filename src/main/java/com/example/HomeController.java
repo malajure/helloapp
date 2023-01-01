5a686b60b6715f551ac283bfde08a72859682fda
malajure <malajure.santosh@gmail.com>
2023-01-01 22:10:04 +0530

    first setup

---
+++ src/main/java/com/example/HomeController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "My Application Code";
+	}
+
+}
